528a926757bd03fc8af59706a22c86cadfa3e226
Dhony Silva <dhony.web@gmail.com>
2025-04-26 15:21:24 -0300

    Adding key bindings to increment event

--- lib/tutorial_web/cookies_live.ex
+++ lib/tutorial_web/cookies_live.ex
@@ -4,7 +4,7 @@ defmodule TutorialWeb.CookiesLive do
   def render(assigns) do
     ~H"""
     <pre>
-      <%= inspect(assigns, pretty: true) %>
+      <%= inspect(@count, pretty: true) %>
     </pre>
     <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-900 to-red-900 text-white">
       <h1 class="absolute top-5 left-5 text-2xl font-bold">Cookie Clicker</h1>
@@ -12,8 +12,35 @@ defmodule TutorialWeb.CookiesLive do
       <div
         class="text-9xl cursor-pointer select-none transition-transform duration-100 hover:scale-110 active:scale-90"
         phx-click="increment"
+        phx-value-amount="1"
+        phx-keydown="increment"
+        phx-key="Enter"
+        tabindex="0"
       >
         🍪
+      </div>
+      <div class="mt-10 space-x-4">
+        <button
+          class="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+          phx-click="increment"
+          phx-value-amount="1"
+        >
+          +1
+        </button>
+        <button
+          class="bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700"
+          phx-click="increment"
+          phx-value-amount="5"
+        >
+          +5
+        </button>
+        <button
+          class="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700"
+          phx-click="increment"
+          phx-value-amount="10"
+        >
+          +10
+        </button>
       </div>
     </div>
     """
@@ -23,8 +50,8 @@ defmodule TutorialWeb.CookiesLive do
     {:ok, assign(socket, count: 0)}
   end
 
-  def handle_event("increment", _params, socket) do
-    new_count = socket.assigns.count + 1
+  def handle_event("increment", %{"amount" => amount}, socket) do
+    new_count = socket.assigns.count + String.to_integer(amount)
     {:noreply, assign(socket, count: new_count)}
   end
 end
