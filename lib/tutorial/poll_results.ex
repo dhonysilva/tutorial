c44926078b609537f8b9a6bae5f76946ecd76551
Dhony Silva <dhony.web@gmail.com>
2025-04-27 17:33:29 -0300

    PollResults with vote and reset methods as the source of truth

--- lib/tutorial/poll_results.ex
+++ lib/tutorial/poll_results.ex
@@ -11,6 +11,14 @@ defmodule Tutorial.PollResults do
     GenServer.call(__MODULE__, :get_state)
   end
 
+  def vote(option) when option in ["A", "B", "C", "D"] do
+    GenServer.cast(__MODULE__, {:vote, option})
+  end
+
+  def reset do
+    GenServer.cast(__MODULE__, :reset)
+  end
+
   # Server callbacks
 
   @initial_state %{
@@ -26,5 +34,14 @@ defmodule Tutorial.PollResults do
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_cast({:vote, option}, state) do
+    new_state = Map.update!(state, option, &(&1 + 1))
+    {:noreply, new_state}
+  end
+
+  def handle_cast(:reset, _state) do
+    {:noreply, @initial_state}
   end
 end
